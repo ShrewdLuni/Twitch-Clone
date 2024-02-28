a548b2dd31a625d61c028bd2a0526bab00bbcdbc
ShrewdLuni <ander113q@gmail.com>
2024-02-28 22:06:20 +0200

    feat: add chat toggle

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -9,6 +9,7 @@ import { cn } from "@/lib/utils";
 
 import { Video } from "./video";
 import { Chat } from "./chat";
+import { ChatToggle } from "./chat-toggle";
 
 interface StreamPlayerProps {
 	user : User & { stream : Stream | null};
@@ -31,6 +32,11 @@ export const StreamPlayer = ({user,stream,isFollowing}: StreamPlayerProps) => {
 
 	return (
 		<>
+      {collapsed && (
+        <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+          <ChatToggle/>
+        </div>
+      )}
       <LiveKitRoom className={cn("grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2")} token={token} serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}>
         <div className="space-y-4 col-span-1 lg:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id}/>
