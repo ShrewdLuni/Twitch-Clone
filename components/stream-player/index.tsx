268c4b5607e9c9750dbdd2ec8eb554868a814768
ShrewdLuni <ander113q@gmail.com>
2024-03-09 17:02:11 +0200

    feat: add about card

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -12,9 +12,10 @@ import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 import { Header, HeaderSkeleton } from "./header";
 import { InfoCard } from "./info-card";
+import { AboutCard } from "./about-cart";
 
 interface StreamPlayerProps {
-	user : User & { stream : Stream | null};
+	user : User & { stream : Stream | null, _count: {followedBy : number}};
 	stream : Stream;
 	isFollowing: boolean;
 };
@@ -39,6 +40,7 @@ export const StreamPlayer = ({user,stream,isFollowing}: StreamPlayerProps) => {
           <Video hostName={user.username} hostIdentity={user.id}/>
           <Header hostName={user.username} hostIdentity={user.id} viewerIdentity={identity} imageUrl={user.imageUrl} isFollowing={isFollowing} name={stream.name}/>
           <InfoCard hostIdentity={user.id} viewerIdentity={identity} name={stream.name} thumbnailUrl={stream.thumbnailUrl}/>
+          <AboutCard hostName={user.username} hostIdentity={user.id} viewerIdentity={identity} bio={user.bio} followedByCount={user._count.followedBy}/>
         </div>
         <div className={cn("col-span-1",collapsed && "hidden")}>
           <Chat viewerName={name} hostName={user.username} hostIdentity={user.id} isFollowing={isFollowing} isChatEnabled={stream.isChatEnabled} isChatDelayed={stream.isChatDelayed} isChatFollowersOnly={stream.isChatFollowersOnly}/>
--- lib/user-service.ts
+++ lib/user-service.ts
@@ -7,8 +7,14 @@ export const getUserByUsername = async (username: string) => {
         },
         include: {
             stream: true,
+            _count: {
+                select:{
+                    followedBy: true,
+                },
+            },
         }
     });
+
 
     return user;
 };
