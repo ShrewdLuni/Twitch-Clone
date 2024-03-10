de1b26cdce24d08911370b024b47cda847bdf456
ShrewdLuni <ander113q@gmail.com>
2024-03-10 18:54:57 +0200

    feat: add home page content & feed service

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,8 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
+import { Results, ResultsSkeleton } from "./_components/results";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <div className='flex flex-col gap-y-4'>
+    <div className='h-full p-8 max-w-screen-2xl mx-auto'>
+      <Suspense fallback={<ResultsSkeleton/>}>
+        <Results/>
+      </Suspense>
     </div>
   )
 }
